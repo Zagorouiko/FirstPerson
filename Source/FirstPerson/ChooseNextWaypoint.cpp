6e7feed0054f787bf1d61dcc106edde5f20e54cc
Zagorouiko <Zagorouiko@gmail.com>
2019-07-06 12:10:43 -0700

    transfered reading blackboard index value from C++

--- Source/FirstPerson/ChooseNextWaypoint.cpp
+++ Source/FirstPerson/ChooseNextWaypoint.cpp
@@ -2,7 +2,12 @@
 
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Index: %i"), Index);
 	return EBTNodeResult::Succeeded;
 }

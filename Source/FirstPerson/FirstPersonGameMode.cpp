54ec438889bb05e30add37554aff9087c201c899
Zagorouiko <Zagorouiko@gmail.com>
2019-07-28 10:30:07 -0700

    fixed file structure depency issues

--- Source/FirstPerson/FirstPersonGameMode.cpp
+++ Source/FirstPerson/FirstPersonGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "FirstPersonGameMode.h"
 #include "FirstPersonHUD.h"
-#include "FirstPersonCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AFirstPersonGameMode::AFirstPersonGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
